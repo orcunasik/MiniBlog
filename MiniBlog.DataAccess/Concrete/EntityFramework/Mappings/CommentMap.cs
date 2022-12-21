49ca4f0586f8876fc1c35a844dd87660bc0f789f
Orçun AŞIK <asikorcun@gmail.com>
2022-12-21 17:56:28 +0300

    Created CommentMap with FluentAPI

---
+++ MiniBlog.DataAccess/Concrete/EntityFramework/Mappings/CommentMap.cs
@@ -0,0 +1,26 @@
+﻿using MiniBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class CommentMap : EntityTypeConfiguration<Comment>
+    {
+        public CommentMap()
+        {
+            HasKey(c => c.Id);
+            Property(c => c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(c => c.Name).IsRequired().HasMaxLength(40);
+            Property(c => c.Mail).IsRequired().HasMaxLength(40);
+            Property(c => c.Text).IsRequired().HasMaxLength(700);
+
+            HasRequired<Blog>(c => c.Blog).WithMany(b => b.Comments).HasForeignKey(c => c.BlogId).WillCascadeOnDelete(false);
+            ToTable("Comments");
+        }
+    }
+}
