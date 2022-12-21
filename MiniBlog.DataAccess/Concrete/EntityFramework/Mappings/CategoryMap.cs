a09af1f4bba6ceb3ddde391cb7eabff6b2e39447
Orçun AŞIK <asikorcun@gmail.com>
2022-12-21 17:47:04 +0300

    Created CategoryMap with FluentAPI

---
+++ MiniBlog.DataAccess/Concrete/EntityFramework/Mappings/CategoryMap.cs
@@ -0,0 +1,21 @@
+﻿using MiniBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class CategoryMap : EntityTypeConfiguration<Category>
+    {
+        public CategoryMap()
+        {
+            HasKey(c => c.Id);
+            Property(c =>c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(c =>c.Name).IsRequired().HasMaxLength(15);
+        }
+    }
+}
