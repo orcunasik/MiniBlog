13dac52112655c68a5388fe42aa6d1519695f8c0
Orçun AŞIK <asikorcun@gmail.com>
2022-12-21 18:08:42 +0300

    Created AboutMap with FluentAPI

---
+++ MiniBlog.DataAccess/Concrete/EntityFramework/Mappings/AboutMap.cs
@@ -0,0 +1,25 @@
+﻿using MiniBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class AboutMap : EntityTypeConfiguration<About>
+    {
+        public AboutMap()
+        {
+            HasKey(a => a.Id);
+            Property(a => a.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(a => a.Content1).IsRequired().HasMaxLength(500);
+            Property(a => a.Content2).IsRequired().HasMaxLength(500);
+            Property(a => a.Image1).IsRequired().HasMaxLength(300);
+            Property(a => a.Image2).IsRequired().HasMaxLength(300);
+            ToTable("Abouts");
+        }
+    }
+}
