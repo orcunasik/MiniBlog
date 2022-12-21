eb04ae30f79bb8398639bd3e97d1b1b991a9ed89
Orçun AŞIK <asikorcun@gmail.com>
2022-12-21 21:36:51 +0300

    Created "Configuration" and "InitialCreate" migration files.

---
+++ MiniBlog.DataAccess/Migrations/202212211828146_InitialCreate.cs
@@ -0,0 +1,117 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Abouts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content1 = c.String(nullable: false, maxLength: 500),
+                        Content2 = c.String(nullable: false, maxLength: 500),
+                        Image1 = c.String(nullable: false, maxLength: 300),
+                        Image2 = c.String(nullable: false, maxLength: 300),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(nullable: false, maxLength: 10),
+                        Password = c.String(nullable: false, maxLength: 25),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 20),
+                        Image = c.String(nullable: false, maxLength: 300),
+                        About = c.String(nullable: false, maxLength: 100),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 100),
+                        Image = c.String(nullable: false, maxLength: 300),
+                        Content = c.String(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                        AuthorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.AuthorId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId)
+                .Index(t => t.CategoryId)
+                .Index(t => t.AuthorId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 15),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 40),
+                        Mail = c.String(nullable: false, maxLength: 40),
+                        Text = c.String(nullable: false, maxLength: 700),
+                        BlogId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Blogs", t => t.BlogId)
+                .Index(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 40),
+                        LastName = c.String(nullable: false, maxLength: 40),
+                        Mail = c.String(nullable: false, maxLength: 40),
+                        Subject = c.String(nullable: false, maxLength: 25),
+                        Message = c.String(nullable: false, maxLength: 300),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Comments", "BlogId", "dbo.Blogs");
+            DropForeignKey("dbo.Blogs", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.Blogs", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.Comments", new[] { "BlogId" });
+            DropIndex("dbo.Blogs", new[] { "AuthorId" });
+            DropIndex("dbo.Blogs", new[] { "CategoryId" });
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Comments");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Blogs");
+            DropTable("dbo.Authors");
+            DropTable("dbo.Admins");
+            DropTable("dbo.Abouts");
+        }
+    }
+}
