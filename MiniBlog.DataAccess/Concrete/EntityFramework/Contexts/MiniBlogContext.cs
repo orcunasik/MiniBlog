3ecfd25b563164e3dc5319dfea8b4905254d2583
Orçun AŞIK <asikorcun@gmail.com>
2022-12-21 17:16:09 +0300

    Created Context class

---
+++ MiniBlog.DataAccess/Concrete/EntityFramework/Contexts/MiniBlogContext.cs
@@ -0,0 +1,27 @@
+﻿using MiniBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class MiniBlogContext : DbContext
+    {
+        public MiniBlogContext() : base("MiniBlogConnectionString")
+        {
+
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+       
+    }
+}
