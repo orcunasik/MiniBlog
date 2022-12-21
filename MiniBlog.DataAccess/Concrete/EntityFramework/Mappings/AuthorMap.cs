da22c0642bbf1fe90d12995f131a9aa309e81e0e
Orçun AŞIK <asikorcun@gmail.com>
2022-12-21 17:44:24 +0300

    Created AuthorMap with FluentAPI

---
+++ MiniBlog.DataAccess/Concrete/EntityFramework/Mappings/AuthorMap.cs
@@ -0,0 +1,23 @@
+﻿using MiniBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class AuthorMap : EntityTypeConfiguration<Author>
+    {
+        public AuthorMap()
+        {
+            HasKey(a => a.Id);
+            Property(a => a.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(a => a.Name).IsRequired().HasMaxLength(20);
+            Property(a => a.About).IsRequired().HasMaxLength(100);
+            Property(a => a.Image).IsRequired().HasMaxLength(300);
+        }
+    }
+}
