e58fbc98a90a343e2ca8b6734383ff75ecbd06d0
Orçun AŞIK <asikorcun@gmail.com>
2022-12-21 17:35:31 +0300

    Created Admin.Map with FluentAPI

---
+++ MiniBlog.DataAccess/Concrete/EntityFramework/Mappings/AdminMap.cs
@@ -0,0 +1,22 @@
+﻿using MiniBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class AdminMap : EntityTypeConfiguration<Admin>
+    {
+        public AdminMap()
+        {
+            HasKey(a => a.Id);
+            Property(a => a.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(a => a.UserName).IsRequired().HasMaxLength(10);
+            Property(a => a.Password).IsRequired().HasMaxLength(25);
+        }
+    }
+}
