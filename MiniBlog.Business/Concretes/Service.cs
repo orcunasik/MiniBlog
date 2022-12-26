fc9a65ad34544d6f2e6121ac62b8e1e1d0356e39
Orçun AŞIK <asikorcun@gmail.com>
2022-12-26 18:20:41 +0300

    The "IService" interface is implemented in the "Service" class.

---
+++ MiniBlog.Business/Concretes/Service.cs
@@ -0,0 +1,58 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.DataAccess.Abstractions;
+using MiniBlog.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Business.Concretes
+{
+    public class Service<T> : IService<T> where T : class, IEntity, new()
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork = null)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public T Add(T entity)
+        {
+            _repository.Add(entity);
+            _unitOfWork.Commit();
+            return entity;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _repository.Remove(entity);
+            _unitOfWork.Commit();
+        }
+
+        public void Update(T entity)
+        {
+            _repository.Update(entity);
+            _unitOfWork.Commit();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _repository.Where(expression);
+        }
+    }
+}
