0321e63de19b8f1fb61a98a5f7f0fa6ddfe92eaa
Orçun AŞIK <asikorcun@gmail.com>
2022-12-26 23:48:59 +0300

    We added our Generic constructs, our DbContext class here using Autofac library.

--- MiniBlog.MVC/Global.asax.cs
+++ MiniBlog.MVC/Global.asax.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Builder;
+using Autofac.Integration.Mvc;
+using MiniBlog.Business.Abstractions;
+using MiniBlog.Business.Concretes;
+using MiniBlog.DataAccess.Abstractions;
+using MiniBlog.DataAccess.Concretes;
+using MiniBlog.DataAccess.Concretes.EntityFramework;
+using MiniBlog.DataAccess.Concretes.EntityFramework.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,10 +18,22 @@ using System.Web.Routing;
 
 namespace MiniBlog.MVC
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterGeneric(typeof(EfGenericRepositoryBase<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+            builder.RegisterType<CategoryService>().As<ICategoryService>().InstancePerLifetimeScope();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().InstancePerLifetimeScope();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+            builder.RegisterType<MiniBlogContext>();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
