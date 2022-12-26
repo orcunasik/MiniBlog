a1c2e4ff01fa2a82bfeebad243e3a4fb58c44a7f
Orçun AŞIK <asikorcun@gmail.com>
2022-12-26 21:18:35 +0300

    A controller named "Tests" has been created.

---
+++ MiniBlog.MVC/Controllers/TestsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniBlog.MVC.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Tests
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
