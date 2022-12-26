df70efe9525a46ca711edf173d4344a59fcb806d
Orçun AŞIK <asikorcun@gmail.com>
2022-12-26 18:13:41 +0300

    Relevant abstraction was created for UnitOfWork structuring.

---
+++ MiniBlog.DataAccess/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+    }
+}
