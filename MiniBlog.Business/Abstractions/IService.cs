6aa8347af0fdf8d148d9109b556f53ab1ac9c9bb
Orçun AŞIK <asikorcun@gmail.com>
2022-12-26 18:17:22 +0300

    Added interface structure for services.

---
+++ MiniBlog.Business/Abstractions/IService.cs
@@ -0,0 +1,20 @@
+﻿using MiniBlog.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Business.Abstractions
+{
+    public interface IService<T> where T : class, IEntity, new()
+    {
+        T GetById(int id);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        IEnumerable<T> GetAll();
+        T Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
