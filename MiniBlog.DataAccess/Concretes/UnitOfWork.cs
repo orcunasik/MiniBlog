07add7ef890c99cd851e4a84b58a87bbbcc55ab6
Orçun AŞIK <asikorcun@gmail.com>
2022-12-26 18:29:25 +0300

    Implemented the "IUnitOfWork" interface.

---
+++ MiniBlog.DataAccess/Concretes/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using MiniBlog.DataAccess.Abstractions;
+using MiniBlog.DataAccess.Concretes.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concretes
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MiniBlogContext _context;
+
+        public UnitOfWork(MiniBlogContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
