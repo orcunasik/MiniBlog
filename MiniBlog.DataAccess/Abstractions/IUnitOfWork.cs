c815a5449e3179106134172ea0c31a768978af87
orcunasik <asikorcun@gmail.om>
2023-01-07 21:57:42 +0300

    UnitOfWork configuration is edited

--- MiniBlog.DataAccess/Abstractions/IUnitOfWork.cs
+++ MiniBlog.DataAccess/Abstractions/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiniBlog.Entities.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace MiniBlog.DataAccess.Abstractions
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        IGenericRepository<T> GetRepository<T>() where T : class, IEntity, new();
         void Commit();
     }
 }
--- MiniBlog.DataAccess/Concretes/UnitOfWork.cs
+++ MiniBlog.DataAccess/Concretes/UnitOfWork.cs
@@ -1,7 +1,12 @@
 ﻿using MiniBlog.DataAccess.Abstractions;
+using MiniBlog.DataAccess.Concretes.EntityFramework;
 using MiniBlog.DataAccess.Concretes.EntityFramework.Contexts;
+using MiniBlog.Entities.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +15,39 @@ namespace MiniBlog.DataAccess.Concretes
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private bool disposed = false;
         private readonly MiniBlogContext _context;
-
         public UnitOfWork(MiniBlogContext context)
         {
             _context = context;
         }
 
+        public IGenericRepository<T> GetRepository<T>() where T : class, IEntity, new()
+        {
+            return new EfGenericRepositoryBase<T>(_context);
+        }
+
         public void Commit()
         {
             _context.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
