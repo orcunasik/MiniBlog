c8f0aedf0b5764a868aa3647d5cb88051be81c69
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:27:18 +0300

    Created

---
+++ MiniBlog.DataAccess/Concretes/EntityFramework/EfSubscribeMailDal.cs
@@ -0,0 +1,18 @@
+﻿using MiniBlog.DataAccess.Abstractions;
+using MiniBlog.DataAccess.Concretes.EntityFramework.Contexts;
+using MiniBlog.Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.DataAccess.Concretes.EntityFramework
+{
+    public class EfSubscribeMailDal : EfGenericRepositoryBase<SubscribeMail>, ISubscribeMailDal
+    {
+        public EfSubscribeMailDal(MiniBlogContext context) : base(context)
+        {
+        }
+    }
+}
