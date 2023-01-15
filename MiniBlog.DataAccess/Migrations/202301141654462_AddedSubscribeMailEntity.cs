20ff16c5e7f91e4bc74af35cac8743178defc1ff
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:08:32 +0300

    Created Migration for SubscribeMails Entity

---
+++ MiniBlog.DataAccess/Migrations/202301141654462_AddedSubscribeMailEntity.cs
@@ -0,0 +1,28 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedSubscribeMailEntity : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.SubscribeMails",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Mail = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Mail, unique: true);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.SubscribeMails", new[] { "Mail" });
+            DropTable("dbo.SubscribeMails");
+        }
+    }
+}
