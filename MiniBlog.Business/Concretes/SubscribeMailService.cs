679bed30b4ca875712e0ec507107d8740f2d1a60
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:14:41 +0300

    Created

---
+++ MiniBlog.Business/Concretes/SubscribeMailService.cs
@@ -0,0 +1,18 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.DataAccess.Abstractions;
+using MiniBlog.Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Business.Concretes
+{
+    public class SubscribeMailService : Service<SubscribeMail>, ISubscribeMailService
+    {
+        public SubscribeMailService(IGenericRepository<SubscribeMail> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+    }
+}
