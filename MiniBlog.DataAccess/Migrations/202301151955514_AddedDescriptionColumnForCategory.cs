1e4e6d85b20bb3ecb3d48ce6d7166c5773b7a687
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:10:08 +0300

    Created "Description" column Migration for CategorytEntity

---
+++ MiniBlog.DataAccess/Migrations/202301151955514_AddedDescriptionColumnForCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedDescriptionColumnForCategory : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "Description", c => c.String(nullable: false, maxLength: 250));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Categories", "Description");
+        }
+    }
+}
