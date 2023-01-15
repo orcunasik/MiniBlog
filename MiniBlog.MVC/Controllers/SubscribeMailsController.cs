c43b43dbaafa148fb349904c12201a6f4d47ca36
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:29:53 +0300

    Created

---
+++ MiniBlog.MVC/Controllers/SubscribeMailsController.cs
@@ -0,0 +1,33 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MiniBlog.MVC.Controllers
+{
+    public class SubscribeMailsController : Controller
+    {
+        private readonly ISubscribeMailService _subscribeMailService;
+
+        public SubscribeMailsController(ISubscribeMailService subscribeMailService)
+        {
+            _subscribeMailService = subscribeMailService;
+        }
+
+        public PartialViewResult AddMail()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult AddMail(SubscribeMail subscribeMail)
+        {
+            _subscribeMailService.Add(subscribeMail);
+            return RedirectToAction("Index", "Blogs");
+        }
+
+    }
+}
