b01d50378ba76a50e0b83b3deadc4d55c795ff8f
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:09:46 +0300

    Created "Date" column Migration for CommentEntity

---
+++ MiniBlog.DataAccess/Migrations/202301151250426_AddedDateColumnForCommentEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedDateColumnForCommentEntity : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Comments", "Date", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Comments", "Date");
+        }
+    }
+}
