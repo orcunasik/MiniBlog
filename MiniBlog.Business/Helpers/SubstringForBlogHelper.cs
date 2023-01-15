8d5e26a57fe2740ddeb76a1b4e55691f54cb735f
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:11:43 +0300

    Created a character-delimiting static class for "Content" fields

---
+++ MiniBlog.Business/Helpers/SubstringForBlogHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MiniBlog.Business.Helpers
+{
+    public static class SubstringForBlogHelper
+    {
+        public static string CharacterDelimiter(this string text, int length)
+        {
+            if (text.Length > length)
+            {
+                return text.Substring(0, length) + "...";
+            }
+            else
+            {
+                return text;
+            }
+        }
+    }
+}
