51466d0dd08b5cd8200d61af0399961efa891dd4
Orçun Aşık <asikorcun@gmail.com>
2023-01-16 02:02:49 +0300

    Added Entity

---
+++ MiniBlog.Entities/Concretes/SubscribeMail.cs
@@ -0,0 +1,15 @@
+﻿using MiniBlog.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Entities.Concretes
+{
+    public class SubscribeMail : IEntity
+    {
+        public int Id { get; set; }
+        public string Mail{ get; set; }
+    }
+}
