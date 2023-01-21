5bb88607be8804c83a0561d17537dc2f5863aac7
Orçun Aşık <asikorcun@gmail.com>
2023-01-21 23:48:08 +0300

    Added controller where Comment operations will be managed by Admin.

---
+++ MiniBlog.MVC/Controllers/AdminControllers/AdminCommentsController.cs
@@ -0,0 +1,33 @@
+﻿using MiniBlog.Business.Abstractions;
+using System.Web.Mvc;
+
+namespace MiniBlog.MVC.Controllers.AdminControllers
+{
+    public class AdminCommentsController : Controller
+    {
+        private readonly ICommentService _commentService;
+        private readonly IAuthorService _authorService;
+
+        public AdminCommentsController(ICommentService commentService, IAuthorService authorService)
+        {
+            _commentService = commentService;
+            _authorService = authorService;
+        }
+
+        public ActionResult Index()
+        {
+            var comments = _commentService.GetAll();
+            return View(comments);
+        }
+        public ActionResult BlogComments(int id)
+        {
+            var blogComments = _commentService.GetCommentByBlog(id);
+            return View(blogComments);
+        }
+        public PartialViewResult Comments()
+        {
+            var comments = _commentService.GetAll();
+            return PartialView(comments);
+        }
+    }
+}
