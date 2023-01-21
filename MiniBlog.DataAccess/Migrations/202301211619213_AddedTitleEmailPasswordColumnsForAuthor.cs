6cf17b1a47cbc4092c45bca64a8336a86c3f207c
Orçun Aşık <asikorcun@gmail.com>
2023-01-21 23:41:50 +0300

    Created Migration for Author model new colums

---
+++ MiniBlog.DataAccess/Migrations/202301211619213_AddedTitleEmailPasswordColumnsForAuthor.cs
@@ -0,0 +1,23 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedTitleEmailPasswordColumnsForAuthor : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Authors", "Email", c => c.String(nullable: false, maxLength: 50));
+            AddColumn("dbo.Authors", "Password", c => c.String(nullable: false, maxLength: 50));
+            AddColumn("dbo.Authors", "Title", c => c.String(nullable: false, maxLength: 20));
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Authors", "Email" );
+            DropColumn("dbo.Authors", "Title");
+            DropColumn("dbo.Authors", "Password");
+            DropColumn("dbo.Authors", "Email");
+        }
+    }
+}
