674b1527d8a4ea07607b58826eab06eab62f7d35
Orçun Aşık <asikorcun@gmail.com>
2023-01-21 23:41:01 +0300

    Added fluentapi restrictions for added Title, Email and Password fields.

--- MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/AuthorMap.cs
+++ MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/AuthorMap.cs
@@ -18,6 +18,9 @@ namespace MiniBlog.DataAccess.Concretes.EntityFramework.Mappings
             Property(a => a.Name).IsRequired().HasMaxLength(20);
             Property(a => a.About).IsRequired().HasMaxLength(100);
             Property(a => a.Image).IsRequired().HasMaxLength(300);
+            Property(a => a.Title).IsRequired().HasMaxLength(20);
+            Property(a => a.Email).IsRequired().HasMaxLength(50);
+            Property(a => a.Password).IsRequired().HasMaxLength(50);
             ToTable("Authors");
         }
     }
