5d5550ec79e053f6fadb166eb315a15d98b7aa7c
Orçun Aşık <asikorcun@gmail.com>
2023-01-21 23:47:54 +0300

    Added controller where Author operations will be managed by Admin.

---
+++ MiniBlog.MVC/Controllers/AdminControllers/AdminAuthorsController.cs
@@ -0,0 +1,50 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.Entities.Concretes;
+using System.Web.Mvc;
+
+namespace MiniBlog.MVC.Controllers.AdminControllers
+{
+    public class AdminAuthorsController : Controller
+    {
+        private readonly IAuthorService _authorService;
+
+        public AdminAuthorsController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Authors()
+        {
+            var authors = _authorService.GetAll();
+            return PartialView(authors);
+        }
+        [HttpGet]
+        public ActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(Author author)
+        {
+            _authorService.Add(author);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            var author = _authorService.GetById(id);
+            return View(author);
+        }
+        [HttpPost]
+        public ActionResult Update(Author author)
+        {
+            _authorService.Update(author);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
