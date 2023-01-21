a66d990530db333d33d56b61baa3036c363b37bc
Orçun Aşık <asikorcun@gmail.com>
2023-01-21 23:39:04 +0300

    Added Title, Email and Password property for Author model.

--- MiniBlog.Entities/Concretes/Author.cs
+++ MiniBlog.Entities/Concretes/Author.cs
@@ -13,6 +13,9 @@ namespace MiniBlog.Entities.Concretes
         public string Name { get; set; }
         public string Image { get; set; }
         public string About { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Title { get; set; }
         public ICollection<Blog> Blogs { get; set; }
     }
 }
