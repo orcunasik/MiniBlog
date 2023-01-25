853e9ebd10bbfac4e6d61bc0f8e51e3ee5ae06d8
Orçun Aşık <asikorcun@gmail.com>
2023-01-25 16:42:01 +0300

    Added IsActive property for fluentapi.

--- MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/CommentMap.cs
+++ MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/CommentMap.cs
@@ -1,11 +1,6 @@
 ﻿using MiniBlog.Entities.Concretes;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniBlog.DataAccess.Concretes.EntityFramework.Mappings
 {
@@ -19,6 +14,7 @@ namespace MiniBlog.DataAccess.Concretes.EntityFramework.Mappings
             Property(c => c.Mail).IsRequired().HasMaxLength(40);
             Property(c => c.Text).IsRequired().HasMaxLength(700);
             Property(c => c.Date).IsRequired();
+            Property(c => c.IsActive).IsRequired();
 
             HasRequired<Blog>(c => c.Blog).WithMany(b => b.Comments).HasForeignKey(c => c.BlogId).WillCascadeOnDelete(false);
             ToTable("Comments");
