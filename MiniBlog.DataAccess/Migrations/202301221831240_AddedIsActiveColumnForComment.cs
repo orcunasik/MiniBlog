f2c4f8576c61c6186883fa922dcb1656f7efaee1
Orçun Aşık <asikorcun@gmail.com>
2023-01-25 16:46:32 +0300

    Migration created for the Comment entity "IsActive" field.

---
+++ MiniBlog.DataAccess/Migrations/202301221831240_AddedIsActiveColumnForComment.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedIsActiveColumnForComment : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Comments", "IsActive", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Comments", "IsActive");
+        }
+    }
+}
