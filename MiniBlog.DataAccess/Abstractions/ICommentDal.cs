72e29544dfc29247a17bf83d81210176331eeff9
Orçun Aşık <asikorcun@gmail.com>
2023-01-25 16:35:07 +0300

    Removed unused "usings".

--- MiniBlog.DataAccess/Abstractions/ICommentDal.cs
+++ MiniBlog.DataAccess/Abstractions/ICommentDal.cs
@@ -2,8 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace MiniBlog.DataAccess.Abstractions
 {
