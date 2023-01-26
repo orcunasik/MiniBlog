e90072c42ece0f6ff8beb7799a596f0fb7e2dc88
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 19:00:48 +0300

    Added "IsActive" column for migration

---
+++ MiniBlog.DataAccess/Migrations/202301252108097_AddedIsActveColumnForBlogEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedIsActveColumnForBlogEntity : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Blogs", "IsActive", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Blogs", "IsActive");
+        }
+    }
+}
