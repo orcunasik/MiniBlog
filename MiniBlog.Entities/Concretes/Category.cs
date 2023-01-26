1961b4071ced162cb11e11e60771c869b4d653ae
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 18:53:46 +0300

    Added "CreateDate" property and Removed unused "usings".

--- MiniBlog.Entities/Concretes/Category.cs
+++ MiniBlog.Entities/Concretes/Category.cs
@@ -1,9 +1,6 @@
 ﻿using MiniBlog.Entities.Abstractions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniBlog.Entities.Concretes
 {
@@ -12,6 +9,7 @@ namespace MiniBlog.Entities.Concretes
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
         public ICollection<Blog> Blogs { get; set; }
     }
 }
