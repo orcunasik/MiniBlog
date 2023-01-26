5f1bda907e44df4d5e952ed71161f807b9c0be5f
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 19:12:03 +0300

    A controller has been created for the Categories to be edited by the admin user.

---
+++ MiniBlog.MVC/Controllers/AdminControllers/AdminCategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.Entities.Concretes;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MiniBlog.MVC.Controllers.AdminControllers
+{
+    public class AdminCategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public AdminCategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Category> categories = _categoryService.GetAll();
+            return View(categories);
+        }
+    }
+}
