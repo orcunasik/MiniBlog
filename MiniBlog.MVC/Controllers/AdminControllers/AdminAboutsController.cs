ff17932516cab4af0e74429ddd3e90fe3dd25f29
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 19:11:31 +0300

    A controller has been created for the About page to be edited by the admin user.

---
+++ MiniBlog.MVC/Controllers/AdminControllers/AdminAboutsController.cs
@@ -0,0 +1,30 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.Entities.Concretes;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MiniBlog.MVC.Controllers.AdminControllers
+{
+    public class AdminAboutsController : Controller
+    {
+        private readonly IAboutService _aboutService;
+
+        public AdminAboutsController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            IEnumerable<About> abouts = _aboutService.GetAll();
+            return View(abouts);
+        }
+
+        [HttpPost]
+        public ActionResult Update(About about)
+        {
+            _aboutService.Update(about);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
