f6a89fbe795d286b8593124fa78cbd7633588945
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 19:01:21 +0300

    Added "CreatedDate" column for migration

---
+++ MiniBlog.DataAccess/Migrations/202301251449228_AddedCreatedDateColumnForCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedCreatedDateColumnForCategory : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "CreatedDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Categories", "CreatedDate");
+        }
+    }
+}
