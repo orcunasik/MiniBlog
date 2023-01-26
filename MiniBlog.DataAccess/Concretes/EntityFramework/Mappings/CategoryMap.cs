9678047f31b22af065198fceaf2c3d6f0cebcd0a
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 19:00:00 +0300

    Added "CreatedDate" property for fluentapi and Removed unused "usings".

--- MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/CategoryMap.cs
+++ MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/CategoryMap.cs
@@ -1,11 +1,6 @@
 ﻿using MiniBlog.Entities.Concretes;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniBlog.DataAccess.Concretes.EntityFramework.Mappings
 {
@@ -17,6 +12,7 @@ namespace MiniBlog.DataAccess.Concretes.EntityFramework.Mappings
             Property(c =>c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
             Property(c =>c.Name).IsRequired().HasMaxLength(15);
             Property(c =>c.Description).IsRequired().HasMaxLength(250);
+            Property(c => c.CreatedDate).IsRequired();
             ToTable("Categories");
         }
     }
