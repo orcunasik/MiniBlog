0c08cca421d1483a3ab3aad24acc5a2c32a4a605
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 18:58:35 +0300

    Removed unused "usings".

--- MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/BlogMap.cs
+++ MiniBlog.DataAccess/Concretes/EntityFramework/Mappings/BlogMap.cs
@@ -1,11 +1,6 @@
 ﻿using MiniBlog.Entities.Concretes;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniBlog.DataAccess.Concretes.EntityFramework.Mappings
 {
