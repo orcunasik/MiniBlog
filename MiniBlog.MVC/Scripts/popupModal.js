c61963ebbc4ca9e740efce03c3cb6228e7da37fa
Orçun Aşık <asikorcun@gmail.com>
2023-01-26 19:20:58 +0300

    Added javascript to show the blog content as PopupModal when going to the corresponding author avatar on the AdminBlogs page.

---
+++ MiniBlog.MVC/Scripts/popupModal.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+     $("body").on("click", ".view", function () {
+            var url = $(this).data("target");
+            $.post(url, function (data) { })
+                .done(function (data) {
+                    $("#modelView .modal-body").html(data);
+                    $("#modelView").modal("show");
+                })
+                .fail(function () {
+                    $("#modelView .modal-body").text("Error!!");
+                    $("#modelView").modal("show");
+                })
+     });
+ })
