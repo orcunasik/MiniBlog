e9f2d3a9caee40515c94c48ca4ce084a01a827f8
Orçun Aşık <asikorcun@gmail.com>
2023-01-27 17:18:51 +0300

    Added AdminMessages for Contact messages.

---
+++ MiniBlog.MVC/Controllers/AdminControllers/AdminMessagesController.cs
@@ -0,0 +1,38 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.Entities.Concretes;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MiniBlog.MVC.Controllers.AdminControllers
+{
+    public class AdminMessagesController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public AdminMessagesController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public ActionResult SendBox()
+        {
+            IEnumerable<Contact> contacts = _contactService.GetAll();
+            return View(contacts);
+        }
+        public ActionResult InBox()
+        {
+            IEnumerable<Contact> contacts = _contactService.GetAll();
+            return View(contacts);
+        }
+        public PartialViewResult MessageSidebar()
+        {
+            return PartialView();
+        }
+        [HttpGet]
+        public ActionResult MessageDetails(int id)
+        {
+            Contact contact = _contactService.GetById(id);
+            return View(contact);
+        }
+    }
+}
