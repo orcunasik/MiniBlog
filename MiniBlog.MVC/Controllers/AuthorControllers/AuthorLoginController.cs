c562228eb982d02a56fda82f9fa02fa71fdda4ed
Orçun Aşık <asikorcun@gmail.com>
2023-02-19 23:30:31 +0300

    Added AuthorLogin controller and View page

---
+++ MiniBlog.MVC/Controllers/AuthorControllers/AuthorLoginController.cs
@@ -0,0 +1,46 @@
+﻿using MiniBlog.Business.Abstractions;
+using MiniBlog.Entities.Concretes;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MiniBlog.MVC.Controllers.AuthorControllers
+{
+    public class AuthorLoginController : Controller
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorLoginController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public ActionResult AuthorLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AuthorLogin(Author author)
+        {
+            var authorValue = _authorService.AuthorLogin(author);
+            if (authorValue != null)
+            {
+                FormsAuthentication.SetAuthCookie(authorValue.Email, false);
+                Session["Email"] = authorValue.Email.ToString();
+                return RedirectToAction("Index", "AuthorProfile");
+            }
+            else
+            {
+                return RedirectToAction(nameof(AuthorLogin));
+            }
+
+        }
+
+        public ActionResult AuthorLogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction(nameof(AuthorLogin));
+        }
+    }
+}
