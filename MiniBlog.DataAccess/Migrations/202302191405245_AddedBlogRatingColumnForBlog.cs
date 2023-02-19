98db6b7922f35a3943398336a3cf63cc15123ced
Orçun Aşık <asikorcun@gmail.com>
2023-02-19 23:27:01 +0300

    Added "BlogRating" column for Blog model and Created Migration files

---
+++ MiniBlog.DataAccess/Migrations/202302191405245_AddedBlogRatingColumnForBlog.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniBlog.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedBlogRatingColumnForBlog : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Blogs", "BlogRating", c => c.Short(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Blogs", "BlogRating");
+        }
+    }
+}
